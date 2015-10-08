f5dd587738798ec9ede417e8877c4786c35096f3
Ira Cooke <iracooke@gmail.com>
2015-10-08 11:13:35 +1100

    Initial Commit

---
+++ R/heatmap-components.R
@@ -0,0 +1,87 @@
+#' Create grid and dendro parts of a heatmap as separate ggplot objects
+#'
+#' @param x matrix of intensity values
+#' @param rlabels a character vector of row labels with length equal to number of rows in x
+#' @param clabels a character vector of column labels with length equal to number of columns in x
+#' @param cold whether to cluster columns and produce a column dendro
+#' @param rowd whether to cluster rows and produce a row dendro
+#' @return a named list with the following objects
+#' \itemize{
+#'  \item tile tiled intensity values
+#'  \item rowd and cold row and column dendrograms
+#' }\item transformed data used to plot tile, rowd and cold
+#' @export
+#' @examples
+#' data(pms)
+#'
+#' hmc <- heatmap_components(x = pms)
+#'
+#' show_ggheatmap(hmc$tile,row_dendro = hmc$rowd,col_dendro = hmc$cold)
+
+heatmap_components <- function(x,rlabels=rownames(x),clabels=colnames(x),cold=TRUE,rowd=TRUE){
+  
+  if ( is.null(rlabels) ) { rlabels <- 1:nrow(x) }
+  if ( is.null(clabels) ) { clabels <- 1:ncol(x) }  
+  
+  colnames(x) <- clabels
+  row.hc <- hclust(dist(x))
+  col.hc <- hclust(dist(t(x)))
+  
+  row.dendro <- dendro_data(as.dendrogram(row.hc),type="rectangle")
+  col.dendro <- dendro_data(as.dendrogram(col.hc),type="rectangle")
+  
+  ## dendro plots
+  col.plot <- ggplot(rescale_dendro(segment(col.dendro))) + geom_segment(aes(x=x, y=y, xend=xend, yend=yend)) 
+  row.plot <- ggplot(rescale_dendro(segment(row.dendro))) + geom_segment(aes(x=x, y=y, xend=xend, yend=yend))
+  
+  ## Scales need expanding so that dendros match central grid
+  #
+  col.plot <- col.plot + scale_x_continuous(expand=c(0.5/ncol(x),0))
+  row.plot <- row.plot + scale_x_continuous(expand=c(0.5/nrow(x),0)) + scale_y_continuous(expand = c(0.01,0))
+  
+  ## Get dendro order and rearrange expression matrix accordingly
+  if ( cold ){
+    col.ord <- col.hc$order
+  } else {
+    col.ord <- 1:ncol(x)
+    col.plot <- NULL
+  }
+  
+  if ( rowd ){
+    row.ord <- row.hc$order
+    row.plot <- row.plot +  coord_flip() 
+  } else {
+    row.ord <- 1:nrow(x)
+    row.plot <- NULL
+  }
+  
+  xx <- x[row.ord,col.ord]
+  rlabels <- rlabels[row.ord]
+  xx = data.frame(rlabels,xx)
+  xxm <- melt(xx,value.name = 'Intensity',id.vars = c('rlabels'),variable.name = 'clabels')
+
+  # Ensure that our row and column orderings are respected by ggplot
+  xxm$rlabels <- factor(xxm$rlabels,levels=xx$rlabels)
+  xxm$clabels <- factor(xxm$clabels,levels=colnames(xx))
+  
+  
+  hmp <- ggplot(xxm,aes(clabels,rlabels)) 
+  hmp <- hmp + geom_tile(aes(fill=Intensity)) + scale_fill_gradient2(na.value="white",low="red",mid="grey90",high="blue") 
+  
+  return(list(tile=hmp,rowd=row.plot,cold=col.plot,data=list(tile=xxm,row.ord=row.ord,col.ord=col.ord)))
+  
+}
+
+
+rescale_dendro <- function(dd){
+  xmin = min(dd$xend,dd$x)
+  xmax = max(dd$xend,dd$x)
+  range=xmax-xmin
+  dd$x <- (dd$x-xmin)*1/range
+  dd$xend <- (dd$xend-xmin)*1/range
+  
+  dd
+}
+
+
+
---
+++ R/show-ggheatmap.R
@@ -0,0 +1,144 @@
+#' Displays ggplot based heatmap composed of multiple ggplot objects
+#'
+#' @param tile ggplot object with a tiled grid of intensity values as produced by heatmap_components
+#' @param row_dendro row dendrogram as produced by heatmap_components
+#' @param col_dendro col dendromgram as produced by heatmap_components
+#' @param lstrip list containing ggplots to display as strips along left axis
+#' @param bstrip list containing ggplots to display as strips along bottom axis
+#' @param cold.height relative height of column dendro
+#' @param rowd.width relative width of row dendro
+#' @param lstrip.width relative width of lstrip plots
+#' @param bstrip.height relative height of bstrip plots
+#' @param laxis.width relative width of left axis
+#' @param baxis.height relative height of bottom axis
+#' @param legend.height relative height of legend
+#' @return a gtable object that can be displayed with grid.draw
+#' @export
+#' @examples
+#'data(pms)
+
+#'hmc <- heatmap_components(x = pms)
+
+#'show_ggheatmap(hmc$tile,row_dendro = hmc$rowd,col_dendro = hmc$cold)
+show_ggheatmap<-function(tile,row_dendro,col_dendro,lstrip=NULL,bstrip=NULL,
+                         cold.height=0.2,rowd.width=0.2,lstrip.width=0.05,bstrip.height=0.05,
+                         laxis.width=0.2,baxis.height=0.1,
+                         legend.height=0.1)
+{ 
+  
+  col_dendro <- col_dendro + theme_dendro()
+  row_dendro <- row_dendro + theme_dendro()
+  
+  tile <- tile + scale_x_discrete(expand=c(0.0, 0)) + theme(legend.position="bottom")
+  
+  legends <- list()
+  legends <- append(legends,list(extract.legend(tile)))
+  legends <- append(legends,lapply(lstrip,extract.legend))
+  legends <- append(legends,lapply(bstrip,extract.legend))
+  
+  n_rows <- length(bstrip) + 2 + as.integer(!is.null(col_dendro)) + length(legends)
+  n_cols <- length(lstrip) + 2 + as.integer(!is.null(row_dendro))
+  
+  pad_top = as.integer(!is.null(col_dendro))
+  pad_bottom <- n_rows - 1 - pad_top
+  
+  
+  hm_parts <- list()
+  
+  # Build up the plot column by column
+  #
+  
+  # First column is the left axis only
+  #
+  
+  laxis_grob <- gtable_filter(ggplotGrob(tile),'axis-l')
+  hm_parts <- append.col.for.grob(laxis_grob,hm_parts,pad_top,pad_bottom)
+  
+  
+  # Next come the lstrip columns
+  #
+  for (strip in lstrip) {
+    append.col.for.grob(extract.panel(strip),hm_parts,pad_top,pad_bottom)
+  }
+  
+  #
+  # The central column
+  #
+  if ( !is.null(col_dendro) ){
+    hm_parts <- append(hm_parts,list(extract.panel(col_dendro)))
+  } else {
+    cold.height=numeric(0)
+  }
+  
+  # Central panel
+  hm_parts <- append(hm_parts,list(extract.panel(tile)))
+  
+  # Bottom strips
+  for (strip in bstrip) {
+    hm_parts <- append(hm_parts,list(extract.panel(strip)))
+  }
+  
+  # Bottom axis
+  baxis_grob <- gtable_filter(ggplotGrob(tile),'axis-b')
+  hm_parts <- append(hm_parts,list(baxis_grob))
+  
+  # Legends
+  for (legend in legends) {
+    hm_parts <- append(hm_parts,list(legend))
+  }
+  
+  # RHS Dendro 
+  if ( !is.null(row_dendro) ){
+    hm_parts <- append.col.for.grob(extract.panel(row_dendro),hm_parts,pad_top,pad_bottom)
+  } else {
+    rowd.width=numeric(0)
+  }
+  
+  lstrip_widths <- rep(lstrip.width/length(lstrip),length(lstrip))
+  bstrip_heights <- rep(bstrip.height/length(bstrip),length(bstrip))
+  
+  panel_width <-  1 - laxis.width - sum(lstrip_widths) - sum(rowd.width)
+  panel_height <- 1 - baxis.height - sum(bstrip_heights) - sum(cold.height)
+  
+  legend_heights <- rep(legend.height/length(legends),length(legends))
+  
+  cwidths = c(laxis.width,lstrip_widths,panel_width,rowd.width)
+  cwidths = cwidths*(1/sum(cwidths))
+  
+  rheights = c(cold.height,panel_height,bstrip_heights,baxis.height,legend_heights)
+  rheights = rheights*(1/sum(rheights))  
+  
+  hm <- gtable_matrix("heatmap",grobs=matrix(hm_parts,ncol=n_cols),
+                      widths = unit(cwidths, "null"),
+                      heights = unit(rheights, "null"))
+  
+  #     browser()
+  
+  hm <- gtable_add_padding(hm,unit(rep(1,len=4),"cm"))
+  
+  grid.newpage()    
+  grid.draw(hm)
+  return(hm)
+}
+
+extract.panel <- function(plot){
+  gtable_filter(ggplotGrob(plot),'panel')  
+}
+
+extract.legend <- function(plot){
+  gtable_filter(ggplotGrob(plot + theme(legend.position="bottom")),'guide-box')
+}
+
+# This is for centre row grobs that need top and bottom padding
+# laxis, lstrip, row_dendro
+#
+append.col.for.grob <- function(grob,hm_list,pad_top,pad_bottom){
+  for (i in seq(1,pad_top,len=pad_top)){
+    hm_list <- append(hm_list,list(nullGrob()))
+  }
+  hm_list <- append(hm_list,list(grob))
+  for (i in seq(1,pad_bottom,len=pad_bottom)) {
+    hm_list <- append(hm_list,list(nullGrob()))
+  }
+  hm_list
+}
