2bb9b0656c24e58f999c27fd8b7cc66d5722fdcd
Ira Cooke <iracooke@gmail.com>
2016-04-25 09:30:46 +1000

    Playing with class structure

--- R/ggheat.R
+++ R/ggheat.R
@@ -1,43 +1,25 @@
+
+setClass("GGHeat",
+         slots = list(data = "ANY",
+                      main_plot = "ggORNULL", #Central ggplot where we show the heatmap itself
+                      row_margin_plots = "list",
+                      col_margin_plots = "list"),
+         prototype = list(data = NULL))
+
+# TODO: Implement the + method so that ggplot functions are passed through to the underlying ggplot object
+setMethod("+", c("GGHeat"), function(e1, e2){})
+
+
 ##' Draw a heatmap from gene expression data
 ##'
 ##' 
 ##' @title ggheat
 ##' @param eset ExpressionSet, matrix or data frame containing gene expression data. Genes as rows and samples as columns
-##' @param ... additional parameters passed to ggplot
-
-ggheat.obj <- setClass("GGHeat",
-                      slots = list(
-                        data = "ANY",
-                        main_plot = "ggORNULL", #Central ggplot where we show the heatmap itself
-                        row_margin_plots = "list",
-                        col_margin_plots = "list"),
-                      prototype = list(data = NULL),
-                      contains = "Cache")
-
-
+##' @param ... additional parameters passed to the underlying ggplot object
 ggheat <- function(eset,...){
-  
+  #
+  # TODO: Here goes code to create the ggplot object for the central view
+  # 
+  new("GGHeat",data=eset)
 }
 
-ggheat <- function(data = NULL, mapping = aes(), ...,
-                   environment = parent.frame()) {
-  UseMethod("ggheat")
-}
-
-ggheat.default <- function(data = NULL, mapping = aes(), ...,
-                           environment = parent.frame()) {
-  ggheat.data.frame(fortify(data, ...), mapping, environment = environment)
-}
-
-ggplot.data.frame <- function(data, mapping = aes(), ...,
-                              environment = parent.frame()) {
-
-  p <- structure(list(
-    data = data,
-    plot_env = environment
-  ), class = c("ggheat","ggplot"))
-  
-  p
-}
-
-is.ggheat <- function(x) inherits(x, "ggheat")
